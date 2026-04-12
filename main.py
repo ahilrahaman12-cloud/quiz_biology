bca476d2589fb84c618c3666b604153d5028c790
ahilrahaman12-cloud <ahilrahaman12@gmail.com>
2026-04-12 14:39:51 +0000

    just a biology quiz

---
+++ main.py
@@ -0,0 +1,96 @@
+print("Welcome to the Biology Quiz")
+print("There are 10 questions , Please type them in all lowercase")
+print("1 mark per question")
+
+print("Q1.What is the fundamental unit of life?")
+ans1 = str(input("Enter the answer\n"))
+if ans1=="cell":
+    print("your answer is correct")
+   
+else:
+    print("WRONG ANSWER")
+
+print("Q2.What type of cell is Bacteria?, Eucaryotic or Prokaryotic")
+ans2 = str(input("Enter the answer\n"))
+
+if ans2 == "prokaryotic":
+    print("Your answer is correct")
+   
+else:
+    print("WRONG ANSWER")    
+
+print("Q3.What is the powerhuse of the cell")
+ans3 = str(input("Enter the answer\n"))
+
+if ans3 == "mitochondria":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q4.What kind of cells are found in humans? Eucaryotic or Prokaryotic")
+ans4 = str(input("Enter the answer\n"))
+
+if ans4 == "eukaryotic":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q5.Is the plasma membrene a selectively permiable membrane?" )
+ans5 = str(input("Enter the answer\n"))
+
+if ans5 == "yes":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q6.What are called the suicide bags of a cell?")
+ans6 = str(input("Enter the answer\n"))
+
+if ans6 == "lysosomes":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q7.What shape is neurons?")
+ans7 = str(input("Enter the answer\n"))
+
+if ans7 == "branched":
+    print ("your answer is correct")
+    
+elif ans7 =="elongated":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q8.What is the other name of bonecells")
+ans8 =  str(input("Enter the answer\n"))
+if ans8 == "osteocytes":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q9.What is the size of RBC")
+ans9 =  str(input("Enter the answer\n"))
+
+if ans9 == "7um":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Q10.What kind of organism is amoeba")
+ans10 = str(input("Enter the answer\n"))
+
+if ans10 == "unicellular":
+    print("your answer is correct")
+    
+else:
+    print("WRONG ANSWER")
+
+print("Thank you for taking the quiz ")
